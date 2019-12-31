1b5bdbcd5fa94015c7f43027e0af31ce56c882bd
Charlie Godfrey <cgodfrey@uw.edu>
2019-12-31 12:16:52 -0800

    auto-commit by quick_update.zsh

--- _site/quick_update.zsh
+++ _site/quick_update.zsh
@@ -5,14 +5,15 @@ bundle exec jekyll build && \
     git push;
 
 cp -r ./_site/* ~/Documents/Web/UWWebsite/;
-cd ~/Documents/Web/UWWebsite/;
+cp -r ./_site/* ~/Documents/Web/godfrey-cw.github.io/docs/;
 
+cd ~/Documents/Web/UWWebsite/;
 git add . && \
     git commit -m "auto-commit by quick_update.zsh" && \
     git push origin && \
     git push live;
 
-cp -r ./_site/* ~/Documents/Web/godfrey-cw.github.io/docs/;
+
 cd ~/Documents/Web/godfrey-cw.github.io/;
 git add . && \
     git commit -m "auto-commit by quick_update.zsh" && \
