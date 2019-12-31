e9ecf21775906821cb40b32c05ce543a5ff106be
Charlie Godfrey <cgodfrey@uw.edu>
2019-12-31 13:12:28 -0800

    changed cv redirect

--- quick_update.zsh
+++ quick_update.zsh
@@ -5,19 +5,11 @@ bundle exec jekyll build && \
     git push;
 
 cp -r ./_site/* ~/Documents/Web/UWWebsite/;
-cp -r ./_site/* ~/Documents/Web/godfrey-cw.github.io/~cgodfrey/;
 
 cd ~/Documents/Web/UWWebsite/;
 git add . && \
     git commit -m "auto-commit by quick_update.zsh" && \
     git push origin && \
     git push live;
-
-
-cd ~/Documents/Web/godfrey-cw.github.io/;
-git add . && \
-    git commit -m "auto-commit by quick_update.zsh" && \
-    git push;
-
 
 
