81dd97348dce17a70db540cb57fd78a104a488a2
Charlie Godfrey <cgodfrey@uw.edu>
2023-03-12 11:34:51 -0500

    auto-commit by quick_update.zsh

--- quick_update.zsh
+++ quick_update.zsh
@@ -1,21 +1,19 @@
 #!/usr/bin/zsh -f
 # build website
-# hack since paths are currently broken
-# jekyll build && \
-~/.gem/ruby/3.0.0/bin/jekyll build && \
+jekyll build && \
     git add . && \
     git commit -m "auto-commit by quick_update.zsh" && \
     git push;
 
 # sync site to server
-cd /home/charlie/Documents/Web/godfrey-cw.github.io;
+cd /home/charlie/Documents/godfrey-cw.github.io;
 
 rsync -a -v --delete --info=progress2 \
       --partial --exclude "/.git" --exclude "/.nojekyll" \
-      /home/charlie/Documents/Web/WebDev/_site/ \
-      /home/charlie/Documents/Web/godfrey-cw.github.io/;
+      /home/charlie/Documents/WebDev/_site/ \
+      /home/charlie/Documents/godfrey-cw.github.io/;
 
 git add . && git commit -m "auto-commit by quick_update.zsh";
 git push;
 
-cd /home/charlie/Documents/Web/WebDev;
+cd /home/charlie/Documents/WebDev;
